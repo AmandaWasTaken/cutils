9c429e893f47fd8e3b900fd3cf09da866193ca6e
AmandaWasTaken <amandasandell02@gmail.com>
2026-05-28 19:45:26 +0300

    init

--- src/cutils.h
+++ src/cutils.h
@@ -4,19 +4,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void _free_resource(void* p);
-static void _close_file(FILE** f);
+#define LOG_BUF_SIZE 1024 
+#define GREEN   "\e[0;32m"
+#define YELLOW  "\e[0;33m"
+#define RED     "\e[0;31m"
+#define DEFAULT "\e[0;0m"
+#define println puts
+
+enum {
+	LOGGER_LOG,
+	LOGGER_WARN,
+	LOGGER_ERROR
+};
+
+void _free_resource(void* p);
+void _close_file(FILE** f);
 
 #define autofree  __attribute((cleanup(_free_resource)))
 #define autoclose __attribute((cleanup(_close_file)))
 
-static size_t _strlen(char* s);
-static void _xorswap_int(int* x, int* y);
-static void _swap_float(float* x, float* y);
+size_t _strlen(char* s);
+void _swap_int(int* x, int* y);
+void _swap_float(float* x, float* y);
 
+#define swap(x, y) _Generic((x), \
+	int*:    _swap_int,			 \
+	float*:  _swap_float		 \
+	double*: _swap_double		 \
+)(x, y)
 
-int swap_endian(int byte);
+int swap_endian(int hex);
 void logger(const char* const s, const int log_level);
-void swap(void* x, void* y, int operand);
 
 #endif
